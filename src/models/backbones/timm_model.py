bb9a43c3d9f4f87a61aab8cb68d2d28cf5d1ab85
haritsahm <haritsahm@gmail.com>
2023-08-20 11:14:12 +0700

    fix timm initializer

--- src/models/backbones/timm_model.py
+++ src/models/backbones/timm_model.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 
 class TimmBackbone(nn.Module):
     def __init__(self, model_name: str, pretrained: bool, features_only=False, **kwargs) -> None:
+        super().__init__()
         self.model = timm.create_model(
             model_name, pretrained=pretrained, features_only=features_only, **kwargs
         )
